f91c74fc6f3f026e5711026e1ab90545257ae841
Mayk Brito <maykbrito@gmail.com>
2021-06-08 17:04:59 -0300

    mobile menu finished

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
+/* Variables */
+const header = document.querySelector('header')
+const nav = header.querySelector('nav')
+const menuButton = header.querySelector('button')
+
 /* WHEN CONTENT LOADED */
 window.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('scroll', stickyMenu)
+  menuButton.addEventListener('click', activateMenu)
 })
 
 /* STICK MENU */
 function stickyMenu() {
-  const header = document.querySelector('header')
   header.classList.toggle('sticky', window.scrollY > 0)
 }
 
 /* MOBILE MENU */
+function activateMenu() {
+  document.body.classList.toggle('no-scroll')
+  nav.classList.toggle('active')
+}
