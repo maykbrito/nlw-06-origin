8bd607a08aec5f66fbae1a429a7e6a11fcc114ba
Mayk Brito <maykbrito@gmail.com>
2021-06-10 01:46:02 -0300

    slider - active menu of visible section

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ const links = document.querySelectorAll('header nav ul li a')
 
 /* WHEN CONTENT LOADED ===================================== */
 window.addEventListener('DOMContentLoaded', function () {
-  window.addEventListener('scroll', () => {
+  window.addEventListener('scroll', e => {
     stickyMenu()
     showBackToTopButton()
+    activateCurrentMenuOfVisibleSection(e)
   })
   menuButton.addEventListener('click', activateMenu)
 })
@@ -39,8 +40,36 @@ links.forEach(link => {
   })
 })
 
-/* SMOOTH SCROLL ============================================ */
+/* HIGHLIGHT CURRENT SECTION MENU =========================== */
 const navHeight = 55
+let lastActiveLink
+let sections = []
+
+for (let section of document.querySelectorAll('section')) {
+  const sectionTop = section.offsetTop - navHeight
+  const sectionId = section.getAttribute('id')
+  const link = document.querySelector(`a[href="#${sectionId}"`)
+
+  sections.push({
+    sectionTop,
+    link
+  })
+}
+
+function activateCurrentMenuOfVisibleSection() {
+  const currentPosition = window.pageYOffset
+
+  sections.forEach(({ sectionTop, link }) => {
+    if (currentPosition >= sectionTop) {
+      lastActiveLink && lastActiveLink.classList.remove('active')
+      link.classList.add('active')
+      lastActiveLink = link
+    }
+  })
+}
+
+/* SMOOTH SCROLL ============================================ */
+// const navHeight = 55
 
 function scrollToTarget(target) {
   const element = document.querySelector(target)
@@ -55,12 +84,21 @@ function smoothScroll(top = 0) {
   window.scroll({ top, behavior: 'smooth' })
 }
 
+/* auto scroll when enter page with hash */
+setTimeout(function () {
+  if (location.hash) {
+    window.scrollTo(0, 0)
+    const target = location.hash.split('?')
+    scrollToTarget(target)
+  }
+}, 1)
+
 /* SMOOTH SCROLL -  BACK TO TOP ============================= */
 const backToTopButton = document.querySelector('#back-to-top')
 
 backToTopButton.addEventListener('click', e => {
   e.preventDefault()
-  smoothScroll('#top')
+  smoothScroll('#home')
 })
 
 function showBackToTopButton() {
@@ -70,3 +108,16 @@ function showBackToTopButton() {
     backToTopButton.classList.remove('show')
   }
 }
+
+/* LIB: TINY SLIDER ============================= 
+https://github.com/ganlanyuan/tiny-slider
+*/
+tns({
+  container: '.my-testimonials',
+  items: 1,
+  slideBy: 'page',
+  mouseDrag: true,
+  controls: false,
+  navPosition: 'bottom',
+  arrowKeys: true
+})
